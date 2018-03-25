68f98a08fcf3c18f9c7f3377599361172e7b2851
Severo <severo.fabricio@gmail.com>
2018-03-25 13:31:24 -0300

    Adds sign in logic

--- app/sign-in/sign-in.component.ts
+++ app/sign-in/sign-in.component.ts
@@ -1,4 +1,8 @@
 import { Component } from "@angular/core";
+import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+
+import { AuthService } from "../shared/auth.service";
 
 @Component({
   selector: 'sign-in',
@@ -8,5 +12,46 @@ import { Component } from "@angular/core";
 })
 
 export class SignInComponent {
+  public form: FormGroup;
+  public submitted: boolean;
+  public formErrors: string[];
 
+  public constructor (
+    private authService: AuthService,
+    private formBuilder: FormBuilder,
+    private router: Router,
+  ) {
+    this.setupForm();
+    this.submitted = false;
+    this.formErrors = null;
+  }
+
+  private setupForm() {
+    this.form = this.formBuilder.group({
+      email: [null, [Validators.required, Validators.email] ],
+      password: [null, [Validators.required] ],
+    });
+  }
+
+  public signInUser() {
+    this.submitted = true;
+
+    this.authService.signIn(this.form.get('email').value, this.form.get('password').value)
+      .subscribe(
+        () => {
+          alert('Login realizado!');
+          this.router.navigate(['/home']);
+          this.formErrors = null;
+        },
+        (error) => {
+          this.submitted = false;
+
+          if (error.status === 401) {
+            this.formErrors = error._body.errors;
+          } else {
+            this.formErrors = ['Não foi possível processar a sua requisição. Por favor, Tente mais tarde.'];
+          }
+        }
+      );
+  }
 }
